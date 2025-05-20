727d66afd4906ec61f4376a577958003f5fa269a
Paul decalf <pauldecalf@outlook.fr>
2025-05-20 23:28:51 +0200

    Add google analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TawktoChat from '../components/TawktoChat';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -71,6 +72,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <GoogleAnalytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
